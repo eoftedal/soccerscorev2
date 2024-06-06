706b0e892a63a05adb825540060dc7b1caa8b59b
Erlend Oftedal <erlend@oftedal.no>
2024-06-06 15:25:00 +0200

    Correct path

--- vite.config.ts
+++ vite.config.ts
@@ -30,6 +30,7 @@ const favIconPlugin = () => {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), favIconPlugin()],
+  base: "/soccerscorev2",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
